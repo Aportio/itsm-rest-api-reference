8a00a4f25654ac1b131e68da65cb00b38696864a
Wade <wwrwade@gmail.com>
2020-06-17 17:59:41 +1200

    Create scraper script and add Root and User GET definitions

---
+++ create_openapi_definition.py
@@ -0,0 +1,112 @@
+from itsm_api import views
+import yaml
+import datetime
+
+
+RESOURCES = [
+    # Root-level resource
+    views.Root,
+
+    # Top-level list resources
+    views.UserList,
+    # views.CustomerList,
+    # views.TicketList,
+    # views.CommentList,
+    # views.CustomerUserAssociationList,
+
+    # # Individual resources
+    # views.User,
+    # views.Customer,
+    # views.Ticket,
+    # views.Comment,
+    # views.CustomerUserAssociation,
+    # # Second-level list resources
+
+    # views.UserCustomerList,
+    # views.CustomerUserList,
+    # views.CustomerTicketList,
+    # views.UserTicketList
+]
+
+
+# Base document definition. Contains metadata such as OpenAPI document version and API
+# information along with the outline for filling in the API endpoint information. Well add
+# more to this to create a full definition.
+openapi_definition = {
+    "openapi": "3.0.1",
+    "info": {
+        "title": "Aportio ITSM REST API reference",
+        "description": "Reference documentation for Aportio's ITSM REST API.",
+        "version": "0.1"
+    },
+    "tags": [
+        {
+            "name": "Root",
+            "description": "The root level of the API"
+        },
+        {
+            "name": "Users",
+            "description": "Requests against user resources"
+        },
+        {
+            "name": "Tickets",
+            "description": "Requests against ticket resources"
+        }
+    ],
+    "paths": {
+    }
+}
+
+
+def add_get_definition(resource):
+    # Get the path to the resource. This will be used as a key in the definition dict.
+    path = resource.URL
+
+    if not openapi_definition["paths"].get(path):
+        openapi_definition["paths"][path] = {}
+
+    # Get the OpenAPI definition from the resource's GET method docstring, and use pyyaml to
+    # load it into python.
+    get_resource_definition_yaml = resource._get.__doc__.split('---')[-1]
+    get_resource_definition      = yaml.load(get_resource_definition_yaml)
+
+    # Update the main OpenAPI definition with the resource's GET method.
+    openapi_definition["paths"][path]["get"] = get_resource_definition
+
+
+def add_post_definition(resource):
+    # Get the path to the resource. This will be used as a key in the definition dict.
+    path = resource.URL
+
+    if not openapi_definition["paths"].get(path):
+        openapi_definition["paths"][path] = {}
+
+    # Get the OpenAPI definition from the resource's GET method docstring, and use pyyaml to
+    # load it into python.
+    post_resource_definition_yaml = resource._post.__doc__.split('---')[-1]
+    post_resource_definition      = yaml.load(post_resource_definition_yaml)
+
+    # Update the main OpenAPI definition with the resource's GET method.
+    openapi_definition["paths"][path]["post"] = post_resource_definition
+
+
+if __name__ == "__main__":
+    # Go through the list of resources that we want to create definitions for.
+    for res in RESOURCES:
+        # If the resource has a GET method, add that to the definition.
+        if "_get" in dir(res):
+            add_get_definition(res)
+
+        # If the resource has a POST method, add that to the definition.
+        if "_post" in dir(res):
+            add_post_definition(res)
+
+    # Get the date and time for right now, then generate a name for the definition file
+    now = datetime.datetime.now()
+    filename = f"Aportio REST API definition {now}"
+
+    # Save the definition to the definition file
+    with open(f"{filename}.yaml", "w") as definition_file:
+        yaml.dump(openapi_definition, definition_file, sort_keys=False)
+
+    print(f"Wrote OpenAPI definition to {filename}")
--- itsm_api/views.py
+++ itsm_api/views.py
@@ -576,6 +576,78 @@ class Root(flask_restful.Resource, ApiResource):
     def _get(self):
         """
         Return the root resource, which includes links to all collections.
+        ---
+        tags:
+        - Root
+        summary: GET Root resource
+        description: Return the root resource, which includes links to all collections.
+        responses:
+          '200':
+            description: Successful GET request. Returns resources.
+            content:
+              application/json:
+                schema:
+                  type: object
+                  properties:
+                    _links:
+                      type: object
+                      properties:
+                        tickets:
+                          type: object
+                          properties:
+                            href:
+                              type: string
+                        comments:
+                          type: object
+                          properties:
+                            href:
+                              type: string
+                        customer_user_associations:
+                          type: object
+                          properties:
+                            href:
+                              type: string
+                        self:
+                          type: object
+                          properties:
+                            href:
+                              type: string
+                        customers:
+                          type: object
+                          properties:
+                            href:
+                              type: string
+                        users:
+                          type: object
+                          properties:
+                            href:
+                              type: string
+                examples:
+                  '0':
+                    value: |
+                      {
+                          "_links": {
+                              "self": {
+                                  "href": "/"
+                              },
+                              "users": {
+                                  "href": "/users"
+                              },
+                              "customers": {
+                                  "href": "/customers"
+                              },
+                              "tickets": {
+                                  "href": "/tickets"
+                              },
+                              "comments": {
+                                  "href": "/comments"
+                              },
+                              "customer_user_associations": {
+                                  "href": "/customer_user_associations"
+                              }
+                          }
+                      }
+
         """
         return {
             "_links" : self.make_links({
@@ -617,6 +689,126 @@ class UserList(flask_restful.Resource, ApiResourceList, _UserDataEmbedder):
     def _get(self, query=None):
         """
         Return the user table data.
+        ---
+        tags:
+        - Users
+        summary: GET Users resource
+        description: Return the user table data.
+        responses:
+          '200':
+            description: Successful GET request. Returns user table data.
+            content:
+              application/json:
+                schema:
+                  type: object
+                  properties:
+                    _embedded:
+                      type: object
+                      properties:
+                        users:
+                          type: array
+                          items:
+                            type: object
+                            properties:
+                              _links:
+                                type: object
+                                properties:
+                                  self:
+                                    type: object
+                                    properties:
+                                      href:
+                                        type: string
+                              _created:
+                                type: string
+                              id:
+                                type: integer
+                              email:
+                                type: array
+                                items:
+                                  type: string
+                    _links:
+                      type: object
+                      properties:
+                        contained_in:
+                          type: object
+                          properties:
+                            href:
+                              type: string
+                        self:
+                          type: object
+                          properties:
+                            href:
+                              type: string
+                    total_queried:
+                      type: integer
+                examples:
+                  '0':
+                    value: |
+                      {
+                          "total_queried": 4,
+                          "_embedded": {
+                              "users": [
+                                  {
+                                      "id": 1,
+                                      "email": [
+                                          "some@user.com"
+                                      ],
+                                      "_created": "",
+                                      "_links": {
+                                          "self": {
+                                              "href": "/users/1"
+                                          }
+                                      }
+                                  },
+                                  {
+                                      "id": 2,
+                                      "email": [
+                                          "another@user.com",
+                                          "with-multiple@emails.com"
+                                      ],
+                                      "_created": "",
+                                      "_links": {
+                                          "self": {
+                                              "href": "/users/2"
+                                          }
+                                      }
+                                  },
+                                  {
+                                      "id": 3,
+                                      "email": [
+                                          "foo@foobar.com"
+                                      ],
+                                      "_created": "",
+                                      "_links": {
+                                          "self": {
+                                              "href": "/users/3"
+                                          }
+                                      }
+                                  },
+                                  {
+                                      "id": 4,
+                                      "email": [
+                                          "someone@somewhere.com"
+                                      ],
+                                      "_created": "",
+                                      "_links": {
+                                          "self": {
+                                              "href": "/users/4"
+                                          }
+                                      }
+                                  }
+                              ]
+                          },
+                          "_links": {
+                              "self": {
+                                  "href": "/users"
+                              },
+                              "contained_in": {
+                                  "href": "/"
+                              }
+                          }
+                      }
+
         """
         if not query:
             user_data = DB_USER_TABLE.all()
